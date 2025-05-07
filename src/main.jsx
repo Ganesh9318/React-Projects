a54ac67f08347f94beb7258ec32af9ceb39c5bb6
Ganesh-Kumar <grai3842@gmail.com>
2025-05-07 14:56:03 +0530

    problem solved of Home component

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import { Component, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Layout from "./Layout/layout/Layout";
@@ -15,16 +15,45 @@ import { Provider } from "react-redux";
 import { Store } from "./Redux/Store/Store";
 import Home from "./Layout/Home/Home.jsx";
 
-const Router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route path="/Home" element={<Home />} />
-      <Route path="/About" element={<About />} />
-      <Route path="/Projects" element={<Projects />} />
-      <Route path="/Contact" element={<Contact />} />
-    </Route>
-  )
-);
+// const Router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<Layout />}>
+//       <Route path="/Home" element={<Home />} />
+//       <Route path="/About" element={<About />} />
+//       <Route path="/Projects" element={<Projects />} />
+//       <Route path="/Contact" element={<Contact />} />
+//     </Route>
+//   )
+// );
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "Home",
+        element: <Home />,
+      },
+      {
+        path: "About",
+        element: <About />,
+      },
+      {
+        path: "Projects",
+        element: <Projects />,
+      },
+      {
+        path: "Contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
